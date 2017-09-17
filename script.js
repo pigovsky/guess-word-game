df8fc3f3ed53594f1a305e28ccc6b19b14884495
Andrew <andrewyakymyshyn@gmail.com>
2017-09-17 11:48:13 +0300

    Added CSS, fixed logic

--- script.js
+++ script.js
@@ -1,112 +1,59 @@
-window.onload = function(){
+var questions = [
+            'Питання 1',
+            'Питання 2',
+            'Питання 3',
+            'Питання 4',
+            'Питання 5',
+            'Питання 6'
+        ]
 
-	var words = [
-    'виелица',
-    'политех',
-    'слово',
-    'стул',
-    'регенерация',
-    'клавиатура',
-    'строка',
-    'монитор',
-    'человек',
-    'нога',
-    'крокодил',
-    'фабрика',
-    'море',
-    'фонтан',
-    'казус',
-    'провод',
-    'аниме',
-    'блоха',
-    'огурец',
-    'процессор',
-    'видеокарта',
-    'книга',
-    'часы',
-    'бутылка',
-    'пиксель',
-    'телефон',
-    'фонограмма',
-    'экран',
-    'щит',
-    'клавиша',
-    'щека',
-    'цыпленок',
-    'больной',
-    'щенок',
-    'часы',
-    'рюкзак',
-    'строка',
-    'номер',
-    'диспетчер',
-    'мышь',
-    'наклейка',
-    'шкаф',
-    'проектор',
-    'кавычки',
-    'скоба',
-    'имя',
-    'скобка'
-];
+        var answers = [
+            'Відповідь 1',
+            'Відповідь 2',
+            'Відповідь 3',
+            'Відповідь 4',
+            'Відповідь 5',
+            'Відповідь 6'
+        ]
 
+        var q = document.getElementById('quest');
+        var btn = document.getElementById('btn');
+        var input = document.getElementById('inp');
 
-var question = document.getElementById('__question');
+        var i = 0;
+        var j = 0;
+        var points = 0;
 
-var lifeAmount = document.getElementById('lifes');
+        q.innerHTML = questions[i];
 
-var input = document.getElementById('ourInput');
+        btn.onclick = function(){
+            setQuestion();
+            input.focus();
+            input.value = '';
+        }
 
-var answer = input.innerHTML;
+        nextStep = function(){
+            i++;
+            j++;
+            q.innerHTML = questions[i];
+            if(i > questions.length - 1){
+                alert('Молодець! Кількість набраних балів: ' + points + '. Спробувати ще раз? ');
+                i = 0;
+            }
+            q.innerHTML = questions[i];
+        }
 
-var button = document.getElementById('submitBtn');
-
-var word = words[Math.floor(Math.random() * words.length) ]; //Беремо будь-яке слово з масиву та рахуємо його довжину
-
-var answerArray = []; 
-
-var lifes = word.length + 10;
-
-for (var i = 0; i < word.length; i++){ //Кожен символ в паличку
-    answerArray[i] = '_'
-}
-
-var remaingLetters = word.length;
-
-question.innerHTML = 'Вгадайте букву! У вас '+ lifes + ' спроб';
-
-var guess = question.innerHTML;
-
-if (remaingLetters > 0 && lifes > 0){
-
-		lifeAmount.innerHTML = answerArray.join(' ');
-
-		button.onclick = function(){
-
-		console.log(1);
-
-		for(var i = 0; i < word.length; i++){
-
-			if (answer === word)
-				{
-					alert(1);
-				}
-		
-			else{
-				alert(2);
-			}
-
-		}
-
-			}
-}
-
-lifeAmount.innerHTML = answerArray.join(' ');
-
-if(lifes > 0){
-    alert('Це слово - ' + word);
-} else {
-    alert('слово ' + word);
-}
-
-};
+        setQuestion = function(){
+            if(input.value.toLowerCase() === answers[j].toLowerCase()){
+                alert('+');
+                console.log(j);
+                points += answers[j].length;
+                q.innerHTML = questions[i+1];
+                nextStep();
+            }
+            else{
+                alert('Помилка! Правильна відповідь: ' + answers[j]);
+                points += 0;
+                nextStep();
+            }
+        }
