fe8c97f986749f3e59e23b7a5fcffb82fc65683e
Julia Franko <julia.franko.j@gmail.com>
2017-09-20 12:33:08 +0300

    add game

---
+++ Task/Task.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Task
+{
+    public struct Task
+    {
+        public Task(
+            String question,
+        String answer)
+        {
+            this.question = question;
+            this.answer = answer;
+        }
+
+        String question;
+        String answer;
+    }
+}
---
+++ Task/TaskProviderImpl.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Task
+{
+    public class TaskProviderImpl : TaskProvider
+    {
+        public Task Get()
+        {
+            return new Task("","");
+        }
+    }
+}
