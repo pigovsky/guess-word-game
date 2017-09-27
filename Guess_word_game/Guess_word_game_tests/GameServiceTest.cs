b354fef442b011ee1dbe7b8ffdf8ab92a3d39a67
t1h4n <t1h4n123@gmail.com>
2017-09-28 01:35:50 +0300

    created simple GameService test using Moq and nUnit

---
+++ Guess_word_game/Guess_word_game_tests/GameServiceTest.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Guess_word_game.Abstractions;
+using Guess_word_game.Models;
+using Guess_word_game.Services;
+using Moq;
+
+namespace Guess_word_game_tests
+{
+    [TestClass]
+    public class GameServiceTest
+    {
+        [TestMethod]
+        public void GetQuestionReturnsNotNUll()
+        {
+            var questionProviderMock = new Mock<IQuestionsProvider>();
+
+            questionProviderMock.Setup(x => x.GetQuestion()).Returns(new Question());
+
+            var gameServiceImpl = new GameService(questionProviderMock.Object);
+
+            Assert.IsNotNull(gameServiceImpl.GetTask());
+        }
+    }
+}
