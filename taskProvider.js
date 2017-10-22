6a63dfaa35b938a819c6a40ff5c13e87ab4f224b
Andrew <andrewyakymyshyn@gmail.com>
2017-10-23 00:54:15 +0300

    Added task provider

---
+++ taskProvider.js
@@ -0,0 +1,13 @@
+        
+    var taskObj = [
+			{question: "question", answer: "answer"},
+			{question: "question1", answer: "answer1"},
+			{question: "question2", answer: "answer2"},
+			{question: "question3", answer: "answer3"},
+			{question: "question4", answer: "answer4"}
+		];
+		   
+         function taskProvider() {
+            question = taskObj[i].question;
+            answer = taskObj[i].answer;
+        }
