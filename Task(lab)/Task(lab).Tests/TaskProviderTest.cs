b415c25f23643b2cc7ea61f7ef4da773f10344e8
Vitali <gimbarr.mykytiuk@gmail.com>
2017-10-24 00:04:28 +0300

    added fourth task

--- Task(lab)/Task(lab).Tests/TaskProviderTest.cs
+++ Task(lab)/Task(lab).Tests/TaskProviderTest.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Web.Script.Serialization;
 
 namespace Task_lab_.Tests
 {
@@ -14,6 +15,24 @@ namespace Task_lab_.Tests
             Assert.IsNotNull(received);
             Assert.IsNotNull(received.Answer);
             Assert.IsNotNull(received.Question);
+        }
+        [TestMethod]
+        public void TaskProviderImpl2Test()
+        {
+            TaskProviderImpl2 taskpr2 = new TaskProviderImpl2();
+            Task t1 = new Task();
+            Task t2 = new Task();
+            try
+            {
+                t1 = taskpr2.get();
+                t2 = taskpr2.get();
+            }
+            catch(ArgumentOutOfRangeException ex)
+            {
+                Assert.Fail(ex.Message);
+            }
+            var js = new JavaScriptSerializer();
+            Assert.AreNotEqual(js.Serialize(t1), js.Serialize(t2));
         }
     }
 }
--- Task(lab)/Task(lab)/Program.cs
+++ Task(lab)/Task(lab)/Program.cs
@@ -3,13 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
 namespace Task_lab_
 {
     class Program
     {
         static void Main(string[] args)
         {
+            TaskProviderImpl2 task = new TaskProviderImpl2();
+            Console.WriteLine(task.get().Question);
+            Console.WriteLine(task.get().Question);
+            Console.WriteLine(task.get().Question);
+            Console.ReadLine();
+
         }
     }
 }
