b7dc1fb7484853ee42bf803bb91c519d9ec0a99a
Vitali <gimbarr.mykytiuk@gmail.com>
2017-10-24 20:31:44 +0300

    the beginning of the devlopment of the game

---
+++ GuessWordGame/GuessWordGame/GuessManager.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuessWordGame
+{
+    public interface IGuessManager
+    {
+        bool GuessWord(string word);
+        string GuessLetter(char letter);
+        void FirstEntrance();
+        int CurrentScore();
+
+    }
+    class GuessManager
+    {
+    }
+}
