4217cce07d6ad960501cd7172c22bb760819dc8e
finkOS <huge.pisos@gmail.com>
2017-11-10 14:23:38 +0200

    better structure of code

---
+++ ForRequest/ForRequest/IGameView.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ForRequest
+{
+    interface IGameView
+    {
+        void ShowGameOver();
+        void ShowQuestion(Task task);
+        void ShowAnswer(string TempStar);
+        void ShowNotification(string message, Color color);
+        void ShowScore(int Score);
+        
+    }
+}
