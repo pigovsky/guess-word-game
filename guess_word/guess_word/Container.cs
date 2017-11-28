5d41abbabc70ff0a1b0f883c4fe03a9bf877c43c
Unknown <Bohdan1234@meta.ua>
2017-11-28 20:50:15 +0200

    Added game "Guess_word"

---
+++ guess_word/guess_word/Container.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace guess_word
+{
+    class Container
+    {
+        public string Quest
+        {
+            get;
+            private set;
+        }
+        public string Answer
+        {
+            get;
+            private set;
+        }
+
+        public Container(string an,string quest)
+        {
+            Answer = an;
+            Quest = quest;
+        }
+        public Container(string[]param)
+        {
+            Answer = param[1];
+            Quest = param[0];
+        }
+    }
+}
