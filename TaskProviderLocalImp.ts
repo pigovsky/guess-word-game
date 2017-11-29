69dc93da5193142c617e05dfe772fe5c67134eea
IliaTaboo <iliataboo@gmail.com>
2017-11-29 12:41:34 +0200

    Add module for import json to ts

---
+++ TaskProviderLocalImp.ts
@@ -0,0 +1,9 @@
+import {TaskProvider} from "./TaskProvider";
+import {Task} from "./Task";
+
+class TaskProviderLocalImpl implements TaskProvider{
+
+	Get(){
+
+	}
+}
---
+++ typings.d.ts
@@ -0,0 +1,5 @@
+declare module "*.json" {
+    const value: any;
+    export default value;
+}
+//module to import json into TypeScript
