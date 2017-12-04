3b05c5e2c772ca054b959083327fdfa66a07edcd
Ira Kalyniak <irysa0098@gmail.com>
2017-12-04 23:20:01 +0200

    new commit

---
+++ Game/Sing in.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MaterialSkin;
+using System.Drawing.Drawing2D;
+using System.Threading;
+
+
+namespace Game
+{
+    public partial class Form1 : MaterialSkin.Controls.MaterialForm
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            var materialSkinManager = MaterialSkinManager.Instance;
+            materialSkinManager.AddFormToManage(this);
+            materialSkinManager.Theme = MaterialSkinManager.Themes.LIGHT;
+            materialSkinManager.ColorScheme = new ColorScheme(Primary.Blue500, Primary.Blue700, Primary.Cyan300, Accent.Cyan100, TextShade.WHITE);
+            this.SetStyle(ControlStyles.SupportsTransparentBackColor, true);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            Thread gamesesion = new Thread(new ThreadStart(StartGame));
+            gamesesion.Start();
+            //Application.Exit();
+
+        }
+      
+        public void StartGame()
+        {
+             this.Invoke(new Action(() =>
+            {
+                new GameGuess().Show();
+            }));
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Application
+        }
+    }
+        
+    }
+
+
+
---
+++ Game/Users.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game
+{
+    class Users
+    {
+        private int id;
+        private string firstName;
+        private string lastName;
+        private Gender gender;
+        private int birthYear;
+        private string login;
+        private string passwordHash;
+        private int score;
+
+
+            
+    }
+}
