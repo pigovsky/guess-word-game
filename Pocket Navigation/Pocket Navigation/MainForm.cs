c9db63d4b6975f84bf39571c540740301af8ecfb
Кульмаицький Андрій <Keniro98@gmail.com>
2017-12-05 20:30:50 +0200

    Add your project Kulmatytski and Lazorko

---
+++ Pocket Navigation/Pocket Navigation/MainForm.cs
@@ -0,0 +1,325 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Pocket_Navigation
+{
+    public partial class MainForm : Form
+    {
+        public string EncryptionProvaider;//Провайдер який використовується для передачі введеного ключа, класу Encryptor.
+        public MainForm()
+        {
+            InitializeComponent();
+            this.BackColor = Color.FromArgb(36, 46, 59);
+            this.panel1.BackColor = Color.FromArgb(26, 32, 46);
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //При запуску програми проводиться провірка файлів
+            string path = @"Data\\Pocket.data";//Шлях до файла в якому зберігається інформаціях про ключ
+            var flen = new FileInfo(path).Length;
+            if (flen != 0)//Якщо файл не пустий 
+            {
+                string Key = (new Decryption()).DecryptionReuslt.ToString();//Декодуємо інформацію, яка збергіається в файлі
+                string[] split = Key.Split(new Char[] { '-' });// Використовуємо "Split" для розділення ключа за допомогою символа "-" 
+
+                foreach (string s in split)
+                {
+                    if (split.Length == 4)//Провіка довжини ключа
+                    {
+                        ActivKod.Text = split[0];//Перша частина ключа до "-"
+                        ActivKod1.Text = split[1];//Частина після першого "-"
+                        ActivKod2.Text = split[2];//Частина після другого "-"
+                        ActivKod3.Text = split[3];//Остання частина ключа "-"
+                        ActivationStatus.Text = "Програму активовано";//Встановлюємо значення тексту Label на формі
+                    }
+                }
+                //Оголошуємо змінні для провірки кнопок на формі
+                string CheckSygicButton;
+                string CheckIGONButton;
+                string CheckIGOPButton;
+                string CheckNavitelButton;
+                //Провіряємо четверту кпопку
+                string PocketInfopath3path = @"Data\\PocketInfopath3.data";
+                var PocketInfopath3flen = new FileInfo(PocketInfopath3path).Length;
+                if (PocketInfopath3flen != 0)
+                {
+                    DecryptionNavitelButton decryptionNavitel = new DecryptionNavitelButton();
+
+                    string NavitelDecryptResult = (new DecryptionNavitelButton()).DecryptionReuslt.ToString();
+                    CheckNavitelButton = NavitelDecryptResult.ToString();
+
+                    if (NavitelDecryptResult == CheckNavitelButton)
+                    {
+                        Navitel_B.Enabled = true;
+                    }
+                }
+                //Провіряэмо першу кнопку
+                string PocketInfopathpath = @"Data\\PocketInfopath.data";
+                var PocketInfopathflen = new FileInfo(PocketInfopathpath).Length;
+                if (PocketInfopathflen != 0)
+                {
+                    DecryptionSygicButton DecryptSygicButton = new DecryptionSygicButton();
+
+                    string SBDecryptResult = (new DecryptionSygicButton()).DecryptionReuslt.ToString();
+                    CheckSygicButton = SBDecryptResult.ToString();
+
+                    if (SBDecryptResult == CheckSygicButton)
+                    {
+                        SygicTruck_B.Enabled = true;
+                    }
+                }
+                //Провіряємо другу кнопку
+                string PocketInfopath1path = @"Data\\PocketInfopath1.data";
+                var PocketInfopath1flen = new FileInfo(PocketInfopath1path).Length;
+                if (PocketInfopath1flen != 0)
+                { 
+                    DecryptionIGONButton DecryptIGONButton = new DecryptionIGONButton();
+
+                string IGONDecryptResult = (new DecryptionIGONButton()).DecryptionReuslt.ToString();
+                CheckIGONButton = IGONDecryptResult.ToString();
+                if (IGONDecryptResult == CheckIGONButton)
+                {
+                    IGONextgen_B.Enabled = true;
+                }
+                }
+                //Провіряємо третю кнопку
+                string PocketInfopath2path = @"Data\\PocketInfopath2.data";
+                var PocketInfopath2flen = new FileInfo(PocketInfopath2path).Length;
+                if (PocketInfopath2flen != 0)
+                { 
+                    DecryptionIGOPButton DecryptIGOPButton = new DecryptionIGOPButton();
+
+                string IGOPDecryptResult = (new DecryptionIGOPButton()).DecryptionReuslt.ToString();
+                CheckIGOPButton = IGOPDecryptResult.ToString();
+
+                if (IGOPDecryptResult == CheckIGOPButton)
+                {
+                    IGOPrimo_B.Enabled = true;
+                }
+                }
+                //Якщо всі кнопки після активації ключа стають активні, забираємо можливість в користувача активувати програму
+                if (SygicTruck_B.Enabled == true && IGONextgen_B.Enabled == true && IGOPrimo_B.Enabled == true && Navitel_B.Enabled == true)
+                {
+                    ActiveBtn.Enabled = false;
+                }
+            }
+            else
+            {
+               ActivationStatus.Text = "Activate your key";
+            }
+        }
+              
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            IGONextgenForm f1 = new IGONextgenForm();
+            f1.Show();
+        }
+
+        private void SygicTruck_B_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            SygicTruckForm f1 = new SygicTruckForm();
+            f1.Show();
+        }
+
+        private void IGOPrimo_B_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            IGOPrimoForm f1 = new IGOPrimoForm();
+            f1.Show();
+        }
+
+        private void Navitel_B_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            NavitelForm f1 = new NavitelForm();
+            f1.Show();
+        }
+
+        private void Exit_Program_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            Process.Start("https://www.facebook.com/OL.Navigation");
+        }
+
+        private void Addition_B_Click(object sender, EventArgs e)
+        {
+            ActivationPanel.Visible = false;
+            InfoPanel.Visible = false;
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+        private void Setings_B_Click(object sender, EventArgs e)
+        {
+            ActivationPanel.Visible = false;
+            if (InfoPanel.Visible == false)
+                InfoPanel.Visible = true;
+            else
+                InfoPanel.Visible = false;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            InfoPanel.Visible = false;
+            if (ActivationPanel.Visible == false)
+                ActivationPanel.Visible = true;
+            else
+                ActivationPanel.Visible = false;
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            Creators f1 = new Creators();
+            f1.Show();
+        }
+
+        private void HideForm_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            //Аналогічна функція, яка описана на "Form Load"
+            string Key = Clipboard.GetText();
+            string[] split = Key.Split(new Char[] { '-' });
+
+            foreach (string s in split)
+            {
+                if (split.Length == 4)
+                {
+                    ActivKod.Text = split[0];
+                    ActivKod1.Text = split[1];
+                    ActivKod2.Text = split[2];
+                    ActivKod3.Text = split[3];
+                    EncryptionProvaider = Key;
+                }
+                else
+                {
+                    MessageBox.Show("Помилка активації ключа!");
+                }
+            }
+        }
+        private void ActiveBtn_Click(object sender, EventArgs e)
+        {
+            //Кнопка активації програми
+
+            //Змінні призначенні для задання значення статуса кнопки для подальшого запису інформації в файл ".data" вразі якщо в веденому ключі є інформація про їхню активацію
+            string NewSBEncrypt = "true";
+            string NewIGONEncrypt = "true";
+            string NewIGOPEncrypt = "true";
+            string NewNEncrypt = "true";
+            
+            string tbS = ActivKod.Text;
+            string tbIN = ActivKod1.Text;
+            string tbIP = ActivKod2.Text;
+            string tbNT = ActivKod3.Text;
+            char[] ar_tbS = tbS.ToCharArray();
+            char[] ar_tbIN = tbIN.ToCharArray();
+            char[] ar_tbIP = tbIP.ToCharArray();
+            char[] ar_tbNT = tbNT.ToCharArray();
+            //Робимо провірку ключа
+            if (ar_tbS[3] == 78)//Якщо n символ ключа відповідає певному символу то відкриваємо доступ до кнопки та передаємо інформацію про активацію
+            {
+                SygicTruck_B.Enabled = true;
+                EncryptionSygicButton EncryptSygicButton = new EncryptionSygicButton(NewSBEncrypt);
+            }
+          
+            if (ar_tbIN[3] == 69)
+            {
+                IGONextgen_B.Enabled = true;
+                EncryptionIGONButton EncryptIGONButton = new EncryptionIGONButton(NewIGONEncrypt);
+            }
+           
+            if (ar_tbIP[3] == 75)
+            {
+                IGOPrimo_B.Enabled = true;
+                EncryptionIGOPButton EncryptIGOPButton = new EncryptionIGOPButton(NewIGOPEncrypt);
+            }
+           
+            if (ar_tbNT[3] == 65)
+            {
+                Navitel_B.Enabled = true;
+                EncryptionNavitelButton EncryptNavitel = new EncryptionNavitelButton(NewNEncrypt);
+            }
+           
+            if (SygicTruck_B.Enabled == true && IGONextgen_B.Enabled == true && IGOPrimo_B.Enabled == true && Navitel_B.Enabled == true)
+            {
+                ActiveBtn.Enabled = false;
+            }
+            Encryption Encryption = new Encryption(EncryptionProvaider);//Закодовуємо інформацію про ключ
+            ActivationPanel.Hide();
+        }
+
+        private void ActivationPanel_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void panel2_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void ActivKod_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ActivKod_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Control && e.KeyCode == Keys.V)
+            {
+                string Key = Clipboard.GetText();
+                string[] split = Key.Split(new Char[] { '-' });
+
+                foreach (string s in split)
+                {
+                    if (split.Length == 4)
+                    {
+                        ActivKod.Text = split[0];
+                        ActivKod1.Text = split[1];
+                        ActivKod2.Text = split[2];
+                        ActivKod3.Text = split[3];
+                        EncryptionProvaider = Key;
+                    }
+                    else
+                    {
+                        MessageBox.Show("Помилка активації ключа!");
+                    }
+                }
+            }
+        }
+
+        private void button2_Click_2(object sender, EventArgs e)
+        {
+                ActivKod.Clear();
+                ActivKod1.Clear();
+                ActivKod2.Clear();
+                ActivKod3.Clear();
+        }
+    }
+}
---
+++ Pocket Navigation/Pocket Navigation/SygicTruckForm.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Pocket_Navigation
+{
+    public partial class SygicTruckForm : Form
+    {
+        // Create the file name string.
+        string filename;
+        // Create the webclient.
+        WebClient client = new WebClient();
+        public SygicTruckForm()
+        {
+            InitializeComponent();
+            BackColor = Color.FromArgb(36, 46, 59);
+            bunifuProgressBar1.BackColor = Color.FromArgb(26, 32, 46);
+
+        }
+
+        private void SygicTruckForm_Load(object sender, EventArgs e)
+        {
+
+        }
+        // Create the download void.
+        private void DownloadFile(string url, string save)
+        {
+            // We need to add an using, so we can download another file after the first one completes. If we don't that it will not work.
+            using (var client = new WebClient())
+            {
+                // Run code every time the download changes.
+                client.DownloadProgressChanged += new DownloadProgressChangedEventHandler(Changed);
+                // Run codes when file download has been completed.
+                client.DownloadFileCompleted += new AsyncCompletedEventHandler(Completed);
+                // Start the download.
+                client.DownloadFileAsync(new Uri(url), save);
+            }
+        }
+
+        private void Changed(object sender, DownloadProgressChangedEventArgs e)
+        {
+            // Update the progress bar.
+            bunifuProgressBar1.Value = e.ProgressPercentage;
+            // Update status label.
+            lblStatus.Text = "Status: downloading...";
+            // Update progress label.
+            lblProgress.Text = "Progress: downloaded " + (e.BytesReceived / 1024d / 1024d).ToString("0.00 MB") + " (" + e.BytesReceived + " bytes) of " + (e.TotalBytesToReceive / 1024d / 1024d).ToString("0.00 MB") + " (" + e.TotalBytesToReceive + " bytes) " + e.ProgressPercentage + "%";
+            // Disable Download button.
+            btnDownload.Enabled = false;
+            // Enable Cancel button.
+            btnCancel.Enabled = true;
+
+        }
+
+        private void Completed(object sender, AsyncCompletedEventArgs e)
+        {
+            // Run download canceled codes.
+            if (e.Cancelled == true)
+            {
+                lblStatus.Text = "Status: download canceled.";
+            }
+            // Run download completed codes.
+            else if (e.Cancelled == false)
+            {
+                lblStatus.Text = "Status: download completed!";
+            }
+            // Enable Download button.
+            btnDownload.Enabled = true;
+            // Disabled Cancel button.
+            btnCancel.Enabled = false;
+            // Reset the progress bar.
+            bunifuProgressBar1.Value = 0;
+        }
+        private void Back_Program_Click(object sender, EventArgs e)
+        {
+            Hide();
+            MainForm f1 = new MainForm();
+            f1.Show();
+        }
+
+        private void HideForm_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+        private void Download_File_Click(object sender, EventArgs e)
+        {
+           
+            string URL1 = "http://my-files.ru/Save/o48z3s/kodovaya tablica ASCII..png";
+            string URL2 = "http://my-files.ru/Save/83pe2r/Actions-edit-clear-locationbar-rtl-icon.png";
+            string URL3 = "http://my-files.ru/Save/llarqc/Black_Settings (1).png";
+            string URL4 = "http://my-files.ru/Save/8tuhw2/igo-logo.png";
+            string URL5 = "http://my-files.ru/Save/7gjrxk/krestik.png";
+            string URL6 = "http://my-files.ru/Save/sfjvyr/sygic-truck-gps-navigation.png";
+            string URL7 = "http://my-files.ru/Save/46mmf5/unnamed.png";
+            string URL8 = "http://my-files.ru/Save/we9wrb/youtube (1).png";
+            try
+            {
+                // Now we are going to create the function, that enter the file name automatically in the savefiledialog.
+                Uri uri1 = new Uri(URL1);
+                // Save the file name to the string.
+                filename = Path.GetFileName(uri1.LocalPath);
+                
+            }
+            catch
+            {
+                // Leave this blank, we don't need an exception message.
+            }
+            try
+                {
+               // FolderBrowserDialog choosecatalog = new FolderBrowserDialog();
+                // Create a savefiledialog so the user can save the file.
+                SaveFileDialog dialog = new SaveFileDialog();
+                // Select the file type as all files.
+                dialog.Filter = "All Files|*.*";
+                    // Write the file name for the user.
+                    dialog.FileName = filename;
+                    // Open the dialog with dialogresult.
+                    DialogResult result = dialog.ShowDialog();
+                // Check if the user has clicked OK.
+
+                if (result == DialogResult.OK)
+                {
+                    if(SygicTruckapkCheckbox1.Checked == true)
+                    { 
+                        // Start the download.
+                        DownloadFile(URL1, dialog.FileName);
+                    }
+                  
+
+                }
+
+            }
+            catch
+                {
+                    // Leave this blank, we don't need an exception message.
+                }
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            client.CancelAsync();
+        }
+        private void webBrowser1_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+
+        }
+        private void bunifuProgressBar1_progressChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void Exit_Program_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void webBrowser1_DocumentCompleted_1(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            string path = Application.StartupPath + @"";
+        }
+    }
+}
